1ff0075d0a6e0db8661d8225a0f718170c519318
maikelekiam <miarcamone@gmail.com>
2016-05-20 22:07:34 -0300

    Capa negocio problemas

---
+++ CapaNegocio/PersonaNego.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDominio;
+using CapaRepositorio;
+
+namespace CapaNegocio
+{
+    //nada
+    public class PersonaNego
+    {
+        PersonaRepo personaRepo = new PersonaRepo();
+
+
+        public IEnumerable<Persona> MostrarPersona()
+        {
+            return personaRepo.MostrarPersona();
+        }
+        public void GuardarPersona(Persona persona)
+        {
+            personaRepo.GuardarPersona(persona);
+        }
+        public IEnumerable<Localidad> MostrarLocalidad()
+        {
+            return personaRepo.MostrarLocalidad();
+        }
+    }
+
+}
