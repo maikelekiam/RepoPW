a5b3b228600d34b60038b37e73b2bbcf32b2a7f8
diego-acosta <diaco1980@gmail.com>
2016-05-26 22:14:10 -0300

    FondoNego GuardarFondo

---
+++ CapaNegocio/FondoNego.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDominio;
+using CapaRepositorio;
+
+namespace CapaNegocio
+{
+    public class FondoNego
+    {
+        FondoRepo fondoRepo = new FondoRepo();
+        public void GuardarFondo(Fondo fondo)
+        {
+            fondoRepo.GuardarFondo(fondo);
+        }
+    }
+}
