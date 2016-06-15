0d37f6ca008454d1697122db1508d65b4dc3fb10
diego-acosta <diaco1980@gmail.com>
2016-06-15 19:10:18 -0300

    Agrego LlenarListaFondos y LlenarListaModalidad a AltaConvocatoria

--- CyT/AltaConvocatoria.aspx.cs
+++ CyT/AltaConvocatoria.aspx.cs
@@ -4,19 +4,46 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using CapaDominio;
+using CapaNegocio;
 
 namespace CyT
 {
     public partial class AltaConvocatoria : System.Web.UI.Page
     {
+        private FondoNego fondoNego = new FondoNego();
+        private ModalidadNego modalidadNego = new ModalidadNego();
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!Page.IsPostBack)
+            {
+                LlenarListaFondos();
+                LlenarListaModalidad();
+            }
         }
 
         protected void btnGuardarConvocatoria_Click(object sender, EventArgs e)
         {
 
+        }
+
+        protected void dgvConvocatoria_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LlenarListaFondos()
+        {
+            ddlFondo.DataSource = fondoNego.MostrarFondos().ToList();
+            ddlFondo.DataValueField = "idFondo";
+            ddlFondo.DataBind();
+        }
+
+        private void LlenarListaModalidad()
+        {
+            ddlModalidad.DataSource = modalidadNego.MostrarModalidades().ToList();
+            ddlModalidad.DataValueField = "idModalidad";
+            ddlModalidad.DataBind();
         }
     }
 }
