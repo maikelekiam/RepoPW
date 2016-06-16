49bbd1d8a4c801bab8d55f4674e3a719dff8f18e
diego-acosta <diaco1980@gmail.com>
2016-06-15 22:30:18 -0300

    Agrego listaTipoDestinatarioNego

---
+++ CapaNegocio/ListaTipoDestinatarioNego.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDominio;
+using CapaRepositorio;
+
+namespace CapaNegocio
+{
+    public class ListaTipoDestinatarioNego
+    {
+        ListaTipoDestinatarioRepo listaTipoDestinatarioRepo = new ListaTipoDestinatarioRepo();
+        public void GuardarListaTipoDestinatario(ListaTipoDestinatario listaTipoDestinatario)
+        {
+            listaTipoDestinatarioRepo.GuardarListaTipoDestinatario(listaTipoDestinatario);
+        }
+
+        public IEnumerable<ListaTipoDestinatario> MostrarListaTipoDestinatarios()
+        {
+            return listaTipoDestinatarioRepo.MostrarListaTipoDestinatarios();
+        }
+    }
+}
