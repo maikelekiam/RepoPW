b1a2834af8cbbe1198f07448ee13bcc9e4b9f9cf
diego-acosta <diaco1980@gmail.com>
2016-06-15 22:25:00 -0300

    Agrego ListaTipoDestinatarioRepo

---
+++ CapaRepositorio/ListaTipoDestinatarioRepo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDominio;
+
+namespace CapaRepositorio
+{
+    public class ListaTipoDestinatarioRepo
+    {
+        public void GuardarListaTipoDestinatario(ListaTipoDestinatario listaTipoDestinatario)
+        {
+            using (ModeloDeDominio modeloDeDominio = new ModeloDeDominio())
+            {
+                modeloDeDominio.Add(listaTipoDestinatario);
+                modeloDeDominio.SaveChanges();
+            }
+        }
+
+        public IEnumerable<ListaTipoDestinatario> MostrarListaTipoDestinatarios()
+        {
+            using (ModeloDeDominio modeloDeDominio = new ModeloDeDominio())
+            {
+                IEnumerable<ListaTipoDestinatario> result = modeloDeDominio.ListaTipoDestinatarios.ToList();
+                return modeloDeDominio.CreateDetachedCopy(result);
+            }
+        }
+    }
+}
