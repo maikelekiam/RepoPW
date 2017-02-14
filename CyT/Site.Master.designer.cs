7359492253f1aec308dae7850b7b2628edd125a4
maikelekiam <miarcamone@gmail.com>
2017-02-14 11:11:01 -0300

    Cambios el Site.master

--- CyT/Site.Master.designer.cs
+++ CyT/Site.Master.designer.cs
@@ -13,15 +13,6 @@ namespace CyT {
     public partial class SiteMaster {
         
         /// <summary>
-        /// pepe control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm pepe;
-        
-        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
